db14593a6c8c422dc079cf77bcba13357fa61c78
student <vetra2000@bk.ru>
2024-12-03 19:47:33 +0700

    1

--- first.py
+++ first.py
@@ -1,15 +1,40 @@
-from PyQt6.QtWidgets import QPushButton, QWidget
-def main():
-    class YellowDots(QWidget):
-        def __init__(self):
-            super().__init__()
-            self.initUI()
+from PyQt6.QtWidgets import QPushButton, QWidget, QApplication, QMainWindow
+from PyQt6.QtGui import QPainter, QColor
+from random import randint
+from PyQt6 import uic
+import sys
 
-        def initUI(self):
-            self.setGeometry(300, 300, 300, 300)
-            self.setWindowTitle('желтые точки')
+
+class YellowDots(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 300)
+        self.yeee = False
+        self.pushButton.clicked.connect(self.yellow)
+
+    def yellow(self):
+        self.yeee = True
+        self.update()
+    def paintEvent(self, event):
+        if self.yeee:
+            qp = QPainter()
+            qp.begin(self)
+            ran = randint(10, 100)
+            ran_cor = (randint(10, 300), randint(10, 300))
+            qp.setBrush(QColor(255, 255, 0))
+            qp.drawEllipse(ran_cor[0], ran_cor[1], ran, ran)
+            qp.end()
+        self.yeee = False
+
 
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = YellowDots()
+    ex.show()
+    sys.exit(app.exec())
